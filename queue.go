6a47bbe5b8485666a6e12deeb39122cbc039b450
MeshAndrey <35770825+MeshAndrey@users.noreply.github.com>
2025-05-07 17:55:37 +0300

    Project: Update queue: Add remove and iterator func

--- queue.go
+++ queue.go
@@ -65,3 +65,46 @@ func (s Queue[T]) Print() {
 		fmt.Println(v)
 	}
 }
+
+func (s *Queue[T]) Remove(t *T) {
+	if t == nil {
+		return
+	}
+
+	if s.Length() == 0 {
+		return
+	}
+
+	newSlice := []T{}
+	found := false
+	for i := 0; i < len(s.data); i++ {
+		if &s.data[i] != t {
+			continue
+		}
+
+		found = true
+
+		if s.Length() == 1 { // if only one at queue
+			newSlice = []T{}
+		} else if i == 0 { // if first
+			newSlice = append(newSlice, s.data[i+1:]...)
+		} else if i == (s.Length() - 1) { // if last
+			newSlice = append(newSlice, s.data[:i]...)
+		} else { // at the middle ?
+			newSlice = append(newSlice, s.data[:i]...)
+			newSlice = append(newSlice, s.data[i+1:]...)
+		}
+
+		break
+	}
+
+	if found {
+		s.data = newSlice
+	}
+}
+
+func (s *Queue[T]) IterateFront(handler func(t *T)) {
+	for i := 0; i < len(s.data); i++ {
+		handler(&s.data[i])
+	}
+}
--- queue_test.go
+++ queue_test.go
@@ -89,3 +89,90 @@ func TestQueueTail(t *testing.T) {
 		t.Errorf("tail != 30")
 	}
 }
+
+func TestQueueRemove(t *testing.T) {
+	q := Queue[int]{}
+
+	q.Push(10)
+	q.Push(20)
+	q.Push(30)
+
+	var (
+		q1 *int
+		q2 *int
+		q3 *int
+	)
+
+	q.IterateFront(func(t *int) {
+		if *t == 20 {
+			q2 = t
+		}
+	})
+
+	if q2 == nil {
+		t.Errorf("q2 == nil")
+	}
+
+	q.Remove(q2)
+
+	q.IterateFront(func(t *int) {
+		if *t == 10 {
+			q1 = t
+		}
+	})
+
+	if q1 == nil {
+		t.Errorf("q1 == nil")
+	}
+
+	q.Remove(q1)
+
+	q.IterateFront(func(t *int) {
+		if *t == 30 {
+			q3 = t
+		}
+	})
+
+	if q3 == nil {
+		t.Errorf("q3 == nil")
+	}
+
+	q.Remove(q3)
+
+	if q.Length() != 0 {
+		t.Errorf("q.Length() != 0")
+	}
+}
+
+func TestIterateFront(t *testing.T) {
+	q := Queue[int]{}
+
+	q.Push(10)
+	q.Push(20)
+	q.Push(30)
+
+	q1, _ := q.Head()
+	q3, _ := q.Tail()
+
+	q1Found := false
+	q.IterateFront(func(t *int) {
+		if *t == q1 {
+			q1Found = true
+		}
+	})
+
+	if !q1Found {
+		t.Errorf("q1Found == false")
+	}
+
+	q3Found := false
+	q.IterateFront(func(t *int) {
+		if *t == q3 {
+			q3Found = true
+		}
+	})
+
+	if !q3Found {
+		t.Errorf("q3Found == false")
+	}
+}
